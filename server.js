ac02e9b05b5ea19af8e538791629b068517fd760
magnus-1 <magnus-1@users.noreply.github.com>
2017-01-07 19:00:46 +0100

    multiplayer test1

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ io.on('connection', function (socket) {
         var snakePos = JSON.stringify(snakeData)
 		console.log('Users Connected ' + snakePos);
 		socket.emit('WELCOME_MESSAGE',snakeData);
+        socket.broadcast.emit('NEWPLAYER_JOINED',snakeData);
 
     });
 
@@ -66,7 +67,8 @@ io.on('connection', function (socket) {
     socket.on('SNAKE_POSITION', function (snakeData) {
         var snakePos = JSON.stringify(snakeData)
         console.log('SNAKE_POSITION = ' + snakePos);
-        socket.emit('SNAKE_POSITION_UPDATE',snakeData);
+        // socket.emit('SNAKE_POSITION_UPDATE',snakeData);
+        socket.broadcast.emit('SNAKE_POSITION_UPDATE',snakeData);
     });
 
 
