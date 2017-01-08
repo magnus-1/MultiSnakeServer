6f4f2a27981b771d3e682a5c5b9a20e5de120e34
magnus-1 <magnus-1@users.noreply.github.com>
2017-01-09 00:22:12 +0100

    code cleaning

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ var dbRequestHandler = require('./dbRequestHandler');
 var serverPort = 5000;
 app.set('port', process.env.PORT || serverPort);
 
-// var clients			= [];
+
 var players = [];
 var lobby = [];
 io.on('connection', function (socket) {
 
-    var currentUser;
+    var currentUser = {};
 
     socket.on('HELLO_SERVER', function (snakeData) {
         var snakePos = JSON.stringify(snakeData)
@@ -140,35 +140,10 @@ io.on('connection', function (socket) {
     });
 
 
-    // socket.on('PLAY', function (data){
-    // 	currentUser = {
-    // 		name:data.name,
-    // 		id:shortId.generate(),
-    // 		position:data.position
-    // 	}
-
-    // 	clients.push(currentUser);
-    // 	socket.emit('PLAY',currentUser );
-    // 	socket.broadcast.emit('USER_CONNECTED',currentUser);
-
-    // });
-
     socket.on('disconnect', function () {
         console.log("User disconnected, och auto deploy works");
 
-        // socket.broadcast.emit('USER_DISCONNECTED',currentUser);
-        // for (var i = 0; i < clients.length; i++) {
-        // 	if (clients[i].name === currentUser.name && clients[i].id === currentUser.id) {
-
-        // 		console.log("User "+clients[i].name+" id: "+clients[i].id+" has disconnected");
-        // 		clients.splice(i,1);
-
-        // 	};
-        // };
-
     });
-
-
 });
 
 
